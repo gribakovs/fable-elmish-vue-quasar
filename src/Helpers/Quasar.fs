c95e11407e4101c06372b0deda64b3640b9f9c31
Ed Ilyin <ed.il@live.com>
2018-05-25 18:44:23 +0300

    minor

--- src/Helpers/Quasar.fs
+++ src/Helpers/Quasar.fs
@@ -3,16 +3,16 @@ open Fable.Core
 open Fable.Import
 open Fable.Helpers
 
-type Component = Options.Component<obj option,obj option,obj option,obj option>
-
+type Component =
+    Options.Component<obj option,obj option,obj option,obj option>
+    
 do JsInterop.importAll "./styles/quasar.styl"
 // do JsInterop.importAll "quasar-extras/animate"
+
 [<Import("default","quasar")>]
-let quasar: U2<Plugin.PluginObject<obj>,Plugin.PluginFunction<obj>> = jsNative
+let quasar: U2<Plugin.PluginObject<obj>,Plugin.PluginFunction<obj>> =
+    jsNative
 
-[<Import("QBtn","quasar")>]
-let QBtn: Component = jsNative
-
+[<Import("QBtn","quasar")>] let QBtn: Component = jsNative
 let el tag = U4.Case2 tag |> Vue.vNode
 let qBtn = el QBtn
-// let qBtn = Vue.el "QBtn"
