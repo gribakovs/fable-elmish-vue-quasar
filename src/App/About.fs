229b74467c008889f40b56f711b48bd3fd76b2b1
Ed Ilyin <ed.il@live.com>
2018-05-26 09:37:25 +0300

    justify buttons

--- src/App/About.fs
+++ src/App/About.fs
@@ -1,4 +1,6 @@
 module App.About
 open Fable.Helpers.Vue
 
-let view = h1 [] [ str "Fable Elmish Vue Quasar Demo" ]
+let view =
+    h1 [ Class [ "q-pa-xl", true ] ]
+        [ str "Fable Elmish Vue Quasar Demo" ]
